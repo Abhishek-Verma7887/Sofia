d9a30c2969254210120120f5aec00f8d8cf0a65a
Souvik Biswas <sbis1999@gmail.com>
2020-06-21 14:35:52 +0530

    Added Auth0 authentication

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth0/flutter_auth0.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:sofia/secrets.dart';
 import 'package:sofia/utils/sign_in.dart';
 import 'package:websafe_svg/websafe_svg.dart';
 
@@ -11,6 +13,18 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  Auth0 auth0;
+
+  bool webLogged;
+  dynamic currentWebAuth;
+
+  @override
+  void initState() {
+    super.initState();
+    webLogged = false;
+    auth0 = Auth0(baseUrl: 'https://$authDomain/', clientId: authClientID);
+  }
+
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
@@ -39,7 +53,90 @@ class _LoginPageState extends State<LoginPage> {
               semanticsLabel: 'Cover Image',
             ),
             _signInButton(),
+            _auth0SignIn(),
           ],
+        ),
+      ),
+    );
+  }
+
+  void showInfo(String text, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(text),
+          content: Text(message),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void webLogin() async {
+    try {
+      var response = await auth0.webAuth.authorize({
+        'audience': 'https://$authDomain/userinfo',
+        'scope': 'openid email offline_access',
+      });
+      DateTime now = DateTime.now();
+      showInfo('Web Login', '''
+      \ntoken_type: ${response['token_type']}
+      \nexpires_in: ${DateTime.fromMillisecondsSinceEpoch(response['expires_in'] + now.millisecondsSinceEpoch)}
+      \nrefreshToken: ${response['refresh_token']}
+      \naccess_token: ${response['access_token']}
+      ''');
+      webLogged = true;
+      currentWebAuth = Map.from(response);
+      setState(() {});
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  Widget _auth0SignIn() {
+    return DecoratedBox(
+      decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
+          color: Colors.white),
+      child: OutlineButton(
+        highlightColor: Color(0xFFffdbb7),
+        splashColor: Color(0xFFffdbb7),
+        onPressed: !webLogged ? webLogin : null,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+        highlightElevation: 0,
+        borderSide: BorderSide(color: Colors.black),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                image: AssetImage("assets/images/auth0_logo.png"),
+                height: 35.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  'Auth0',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.red,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
