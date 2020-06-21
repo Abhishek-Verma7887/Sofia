781b69ce9fa6e6df123e761205555e6a4fa1f028
Souvik Biswas <sbis1999@gmail.com>
2020-06-21 11:58:43 +0530

    Update voice assistant speech

--- lib/speech/output_speech.dart
+++ lib/speech/output_speech.dart
@@ -7,10 +7,20 @@ final String askToStartWithBeginners =
     'Do you want to start with the beginners track?';
 
 // For starting with the track
-final String startWithTrack = 'Starting with the beginners track...';
+final String startWithTrack = 'Starting with the Triangle pose...';
 
 // Asking to explore
 final String exploreTracks = 'You can explore more tracks on this page';
 
+final String exploreTracksCompletion = 'You can explore more tracks on the next page';
+
 // Basic speech input recognition
 final String notRecognized = 'I didn\'t quite get that. Can you please repeat?';
+
+// After completion of a pose
+final String poseCompletion = 'Do you want to continue to the next pose?';
+
+// Returning back to the home screen
+final String oneCompletion = 'You have received 10 stars, for completing one out of seven poses of the beginners track.';
+
+final String oneCompletionString = 'You have received ⭐10, for completing 1/7 poses of the Beginners track.';
